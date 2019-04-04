dac3505e0a90c387c118c2f60cc6b34b6a858ee5
lottege <siEwu6xy>
2019-04-04 17:39:19 +0200

    exit

--- wheelchair/Version1/pycode.py
+++ wheelchair/Version1/pycode.py
@@ -102,7 +102,7 @@ def check_movement(rotation_values):
     print("point count:", points)
     if is_first_value == True:
         first_values = rotation_values
-        random_movement = random.randint(0,1)
+        random_movement = random.randint(0,3)
         is_first_value = False
         print(first_values)
 
@@ -136,7 +136,7 @@ def check_movement(rotation_values):
             # global first_values_orientation
             # global orientation_values
             # first_values_orientation = orientation_values
-            random_movement = random.randint(0,1)
+            random_movement = random.randint(0,3)
 
     elif random_movement == 1:
         print ("move BACKWARD")
@@ -150,18 +150,18 @@ def check_movement(rotation_values):
             # global first_values_orientation
             # global orientation_values
             # first_values_orientation = orientation_values
-            random_movement = random.randint(0,1)
+            random_movement = random.randint(0,3)
             # End own code
 
     else :
-        return
+        exit(0)
 
 def check_movement_orientation(orientation_values):
     global is_first_value_orientation, first_values_orientation, points
     print("point count:", points)
     if is_first_value_orientation == True:
         first_values_orientation = orientation_values
-        random_movement = random.randint(2,3)
+        random_movement = random.randint(0,3)
         is_first_value_orientation = False
         print(first_values)
 
@@ -195,7 +195,7 @@ def check_movement_orientation(orientation_values):
             # global rotation_values
             first_values_orientation = orientation_values
             # first_values = rotation_values
-            random_movement = random.randint(2,3)
+            random_movement = random.randint(0,3)
 
     elif random_movement == 3:
         print ("turn LEFT")
@@ -206,10 +206,10 @@ def check_movement_orientation(orientation_values):
             global points
             points+=1
             first_values_orientation = orientation_values
-            random_movement = random.randint(2,3)
+            random_movement = random.randint(0,3)
             # End own code
     else :
-        return
+        exit(0)
 
 # Instantiate a thing with its credential, then read its properties from the DCD Hub
 my_thing = Thing(thing_id=THING_ID, token=THING_TOKEN)
